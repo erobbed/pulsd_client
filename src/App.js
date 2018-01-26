28528c618d08faef07e0712229528bf8380aa3d4
Ethan Roberts <ethan.reid.roberts@gmail.com>
2018-01-26 14:38:30 -0500

    added video background and stylings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FormContainer />
+        <FormContainer className="content" />
+        <iframe
+          id="myVideo"
+          title="background"
+          src="https://www.youtube.com/embed/zKNMiuFVmNE?rel=0&amp;controls=0&amp;showinfo=0&amp;start=33&autoplay=1&mute=1"
+          frameBorder="0"
+          allowFullScreen
+        />
       </div>
     );
   }
--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -32,34 +32,38 @@ class FormContainer extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <Form style={{ width: "50%", margin: "0 auto" }}>
-        <Form.Field>
-          <Form.Input
-            label="Event Name"
-            onChange={this.handleChange}
-            value={this.state.name}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Event Times</label>
-          <DatePicker
-            selected={this.state.startDate}
-            onChange={this.handleChangeStart}
-            showTimeSelect
-            dateFormat="LLL"
-          />
-          <DatePicker
-            selected={this.state.endDate}
-            selectsEnd
-            startDate={this.state.startDate}
-            endDate={this.state.endDate}
-            onChange={this.handleChangeEnd}
-            showTimeSelect
-            dateFormat="LLL"
-          />
-        </Form.Field>
-        <Button type="submit">Submit</Button>
-      </Form>
+      <div>
+        <h1 className="form">Create a New Event</h1>
+        <Form className="form">
+          <Form.Field>
+            <Form.Input
+              label="Event Name"
+              onChange={this.handleChange}
+              value={this.state.name}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>Event Start</label>
+            <DatePicker
+              selected={this.state.startDate}
+              onChange={this.handleChangeStart}
+              showTimeSelect
+              dateFormat="LLL"
+            />
+            <label>Event End</label>
+            <DatePicker
+              selected={this.state.endDate}
+              selectsEnd
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              onChange={this.handleChangeEnd}
+              showTimeSelect
+              dateFormat="LLL"
+            />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </div>
     );
   }
 }
